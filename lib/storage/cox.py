e48b3067c3a08d5b94c5eac32eb566f1b86e0c53
Evgeny Safronov <division494@gmail.com>
2013-09-26 13:33:20 +0400

    Working on cocaine storage compatibility.

--- lib/storage/cox.py
+++ lib/storage/cox.py
@@ -2,23 +2,31 @@ import cache
 from . import Storage
 
 from cocaine.services import Service
-
+from cocaine.exceptions import ServiceError, ChokeEvent
 
 class CocaineStorage(Storage):
     def __init__(self, config):
         self._config = config
         self._storage = Service('storage')
-        self.timeout = 1.0
+        self.timeout = 120.0
 
     @cache.get
     def get_content(self, path):
         collection, separator, key = path.partition('/')
-        return self._storage.read(collection, key).get(timeout=self.timeout)
+	print "============get_content", collection, key
+	try:
+            return self._storage.read(collection, key).get(timeout=self.timeout)
+	except ServiceError as err:
+	    raise IOError(err)
 
     @cache.put
     def put_content(self, path, content):
         collection, separator, key = path.partition('/')
-        self._storage.write(collection, key, content, ('docker',)).get(timeout=self.timeout)
+        print "============put_content", collection, key
+	try:
+            self._storage.write(collection, key, content, ('docker',)).get(timeout=self.timeout)            
+	except ChokeEvent:
+	    pass	
         return path
 
     def stream_read(self, path):
@@ -42,6 +50,7 @@ class CocaineStorage(Storage):
         else:
             collection, separator, key = path.partition('/')
 
+        print "=================list", collection, path
         items = self._storage.find(collection, ('docker',)).get(timeout=self.timeout)
         if not items:
             raise OSError('No such directory: \'{0}\''.format(path))
@@ -51,12 +60,19 @@ class CocaineStorage(Storage):
 
     def exists(self, path):
         collection, separator, key = path.partition('/')
+        print "=====================exists", collection, key
         return len(self._storage.find(collection, ('docker',)).get(timeout=self.timeout)) > 0
 
     @cache.remove
     def remove(self, path):
         collection, separator, key = path.partition('/')
-        self._storage.remove(collection, key).get(timeout=self.timeout)
+        print "==========================remove", collection, key
+        try:
+	    self._storage.remove(collection, key).get(timeout=self.timeout)
+	except ChokeEvent:
+	    pass
+	except ServiceError:
+	    pass
 
     def get_size(self, path):
         return len(self.get_content(path))
